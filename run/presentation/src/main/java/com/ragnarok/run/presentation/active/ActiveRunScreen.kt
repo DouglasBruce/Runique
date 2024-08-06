40c53aaa8a65cdf8233861b37cbcfb8203af811f
DouglasBruce <douglasbruce96@gmail.com>
2024-08-07 00:19:16 +0100

    Implement paused run dialog and resume functionality. Resolved bug where new polylines on resume would join previous polylines

--- run/presentation/src/main/java/com/ragnarok/run/presentation/active/ActiveRunScreen.kt
+++ run/presentation/src/main/java/com/ragnarok/run/presentation/active/ActiveRunScreen.kt
@@ -26,6 +26,7 @@ import androidx.compose.ui.unit.dp
 import com.ragnarok.core.presentation.designsystem.RuniqueTheme
 import com.ragnarok.core.presentation.designsystem.StartIcon
 import com.ragnarok.core.presentation.designsystem.StopIcon
+import com.ragnarok.core.presentation.designsystem.components.RuniqueActionButton
 import com.ragnarok.core.presentation.designsystem.components.RuniqueDialog
 import com.ragnarok.core.presentation.designsystem.components.RuniqueFloatingActionButton
 import com.ragnarok.core.presentation.designsystem.components.RuniqueOutlinedActionButton
@@ -157,6 +158,30 @@ private fun ActiveRunScreen(
                     .fillMaxWidth()
             )
         }
+    }
+    
+    if (!state.shouldTrack && state.hasStartedRunning) {
+        RuniqueDialog(
+            title = stringResource(id = R.string.running_is_paused),
+            onDismiss = { onAction(ActiveRunAction.OnResumeRunClick) },
+            description = stringResource(id = R.string.resume_or_finish_run),
+            primaryButton = {
+                RuniqueActionButton(
+                    text = stringResource(id = R.string.resume),
+                    isLoading = false,
+                    onClick = { onAction(ActiveRunAction.OnResumeRunClick) },
+                    modifier = Modifier.weight(1f)
+                )
+            },
+            secondaryButton = {
+                RuniqueOutlinedActionButton(
+                    text = stringResource(id = R.string.finish),
+                    isLoading = state.isSavingRun,
+                    onClick = { onAction(ActiveRunAction.OnFinishRunClick) },
+                    modifier = Modifier.weight(1f)
+                )
+            }
+        )
     }
 
     if (state.showLocationRationale || state.showNotificationRationale) {
