301d9d744974e445f83b2fbded4ba0188135d173
DouglasBruce <douglasbruce96@gmail.com>
2024-08-06 21:40:23 +0100

    Create location observer and run tracking functionality

--- run/presentation/src/main/java/com/ragnarok/run/presentation/di/RunPresentationModule.kt
+++ run/presentation/src/main/java/com/ragnarok/run/presentation/di/RunPresentationModule.kt
@@ -1,11 +1,14 @@
 package com.ragnarok.run.presentation.di
 
+import com.ragnarok.run.domain.RunningTracker
 import com.ragnarok.run.presentation.active.ActiveRunViewModel
 import com.ragnarok.run.presentation.overview.RunOverviewViewModel
 import org.koin.androidx.viewmodel.dsl.viewModelOf
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
-val runViewModelModule = module {
+val runPresentationModule = module {
+    singleOf(::RunningTracker)
     viewModelOf(::RunOverviewViewModel)
     viewModelOf(::ActiveRunViewModel)
 }
